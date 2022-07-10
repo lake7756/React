24720995113124d2fdc4e4445603bcda9aaceb88
Suruchi <suruchi.@Suruchis-MacBook-Air.local>
2022-07-10 18:43:02 +0530

    Expense Tracker project

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpenseList from "./ExpenseList";
+import ExpenseChart from "./ExpenseChart";
 
 import "./Expenses.css";
 
@@ -12,14 +13,17 @@ const Expenses = (props) => {
     filteredYearHandler(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter(item => item.date.getFullYear().toString() === filteredYear)
-   
+  const filteredExpenses = props.items.filter(
+    (item) => item.date.getFullYear().toString() === filteredYear
+  );
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onSelectYear={seletedYearHandler}
       />
+      <ExpenseChart expenses={filteredExpenses} />
       <ExpenseList expenses={filteredExpenses} />
     </Card>
   );
--- src/components/newExpense/NewExpense.js
+++ src/components/newExpense/NewExpense.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseData = (enteredExpenseData) => {
-    const expenseData = { ...enteredExpenseData, id: Math.random().toString() }
+    const expenseData = { ...enteredExpenseData, id: Math.random().toString() };
     props.onAddExpenseData(expenseData);
-  }
+  };
+
+  const isEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditing = () => {
+    setIsEditing(false);
+  };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseData} />
+      {!isEditing && <button onClick={isEditingHandler}>Add Expense</button>}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseData}
+          onCancel={stopEditing}
+        />
+      )}
     </div>
   );
 };
